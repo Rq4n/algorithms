a5cc378e0c7a816f6f37ae01b48e2da7580e44b3
Rq4n <ryanlimabatista2@gmail.com>
2025-07-14 16:35:42 -0300

    selection sort

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -1,15 +1,17 @@
 public class SelectionSort {
-    public static Integer selectionSort(int[] arr) {
-        int menor = arr[0];
-        int menor_indice = 0;
-
+    public static void selectionSort(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] < menor){
-                menor = arr[i];
-                menor_indice = i;
-                return menor_indice;
+            int minIndex = i;
+            for (int j = 1 + i; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            if (minIndex != i){
+                int tmp =  arr[i];
+                arr[i] = arr[minIndex];
+                arr[minIndex] = tmp;
             }
         }
-        return null;
     }
 }
